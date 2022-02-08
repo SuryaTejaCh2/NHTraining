e7715ca3691eaae397f351f10aa34c3f21ffe1f8
SuryaTejaCh2 <suryatejach2@gmail.com>
2022-02-08 17:54:32 +0530

    Feb8

--- Feb7Morning/PropWithPrivate/Program.cs
+++ Feb7Morning/PropWithPrivate/Program.cs
@@ -13,18 +13,25 @@ namespace PropWithPrivate
 {
     class Employee
     {
+        string designation;
         public int Id { get { return Id; } set { Id = value; } }
         public string Name { get { return Name; } set { Name = value; } }
-        public string Designation { get { return Designation; } set { Designation = "S"; } }
+        public string Designation { set { designation = value; } }
         public int Salary 
         { 
-            get { return Salary; } set { Salary = value; } }
+            get 
+            { 
+                return (designation == "S") ? 30000 : 60000; 
+            } set { Salary = value; } }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Employee emp = new Employee();
+            Console.Write("Enter Manager - M or Employee - S: ");
+            emp.Designation = Console.ReadLine();
+            Console.WriteLine($"Salary is {emp.Salary}");
 
             Console.ReadLine();
         }
---
+++ Feb8Morning/Division/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**********************************************************************
+* Author: Surya Teja
+* Purpose: Divison of two numbers using try, catch and finally block
+* *********************************************************************/
+
+namespace Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber, div;
+
+            try
+            {
+                Console.Write("Enter First Number:");
+                firstNumber = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter Second Number:");
+                secondNumber = Convert.ToInt32(Console.ReadLine());
+
+                div = firstNumber / secondNumber;
+
+                Console.WriteLine($"Divison is :{div}");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine($"Enter number range from {int.MinValue} to {int.MaxValue}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Enter second number as other than zero");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Enter only numbers");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please contact Office");
+            }
+            finally
+            {
+                Console.WriteLine("\n\n\n\n\nDesigned by Surya");
+                Console.ReadLine();
+            }
+        }
+    }
+}
