f87d2262a49770e0cdeaf6860e256bd55e4257d0
SuryaTejaCh2 <suryatejach2@gmail.com>
2022-02-11 17:54:33 +0530

    Feb11

---
+++ Feb11/FileTypes/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/**********************************************************************
+* Author: Surya Teja
+* Purpose: File methods
+* *********************************************************************/
+
+namespace FileTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //File Location
+            string fileName = "S:\\NB\\Assi\\Day1 Morning assignment by Surya Teja Chandolu 24 Jan 2022\\C#\\Feb11\\ExampleFiles\\Sample.txt";
+            string fileName1 = "S:\\NB\\Assi\\Day1 Morning assignment by Surya Teja Chandolu 24 Jan 2022\\C#\\Feb11\\ExampleFiles\\Destination\\Sample.txt";
+            
+            // Check File Exist or not and Delete the file     
+            if (File.Exists(fileName))
+                File.Delete(fileName);
+            Console.WriteLine("*****File Deleted*****\n");
+
+            // Create a new file and add text
+            StreamWriter sw = File.CreateText(fileName);
+            sw.WriteLine("Hi All");
+            sw.WriteLine("AWelcome to");
+            sw.WriteLine("Nations Benefit");
+            sw.Close();
+            Console.WriteLine("\n*****Text Added*****");
+            
+            // Read File     
+            StreamReader sr = File.OpenText(fileName);
+            string s = "";
+            while ((s = sr.ReadLine()) != null)
+                Console.WriteLine(s);
+            Console.WriteLine("\n*****Read Line*****");
+            sr.Close();
+            
+
+            if (File.Exists(fileName1))
+                File.Delete(fileName1);
+            File.Move(fileName, fileName1);
+            Console.WriteLine("\n*****File was moved*****");
+
+            //Copy File
+            File.Copy(fileName1, fileName);
+            Console.WriteLine("\n*****File was Copied*****");
+            object date = File.GetCreationTime(fileName1);
+            Console.WriteLine("\n*****Get Creation Time*****");
+            Console.WriteLine(date);
+
+            //Open File
+            File.OpenText(fileName1).Close();
+            Console.WriteLine("\nFile Opened");
+
+            //Append new Text
+            sw = File.AppendText(fileName1);
+            sw.WriteLine("This");
+            sw.WriteLine("is Extra");
+            sw.WriteLine("Text");
+            sw.Close ();
+            Console.WriteLine("\nFile Append");
+
+            //Read File
+            object read = File.ReadAllText(fileName1);
+            Console.WriteLine(read);
+            Console.WriteLine("\nRead Appended File");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Feb11/QuizApplication/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "S:\\NB\\Assi\\Day1 Morning assignment by Surya Teja Chandolu 24 Jan 2022\\C#\\Feb11\\ExampleFiles\\Score.txt";
+
+            int score = 0, answer;
+            string name;
+
+            Console.Write("Enter your Name: ");
+            name =  Console.ReadLine();
+
+            Console.WriteLine("**************************************");
+            Console.WriteLine($"Hi {name} welcome to quiz by Surya");
+            Console.WriteLine("**************************************");
+
+            Console.WriteLine("\nQ1. What is the national game of India ");
+            Console.WriteLine("1.Cricket 2.Field Hockey 3.Badminton 4.Kabaddi");
+            Console.Write("Enter your choice: ");
+            answer = Convert.ToInt32(Console.ReadLine());
+            if (answer == 2)
+                score += 20;
+
+            Console.WriteLine("\nQ2. What is the national game of USA ");
+            Console.WriteLine("1.Tennis 2.Ice Hockey 3.Baseball 4.Football");
+            Console.Write("Enter your choice: ");
+            answer = Convert.ToInt32(Console.ReadLine());
+            if (answer == 3)
+                score += 20;
+
+            Console.WriteLine("\nQ3. What is the national game of Israel ");
+            Console.WriteLine("1.Swimming 2.Field Hockey 3.Basketball 4.Football");
+            Console.Write("Enter your choice: ");
+            answer = Convert.ToInt32(Console.ReadLine());
+            if (answer == 4)
+                score += 20;
+
+            Console.Write("\nQ4. What is the national game of Scotland: ");
+            Console.WriteLine("1.Golf 2.Tennis 3.Football 4.Rugby");
+            Console.Write("Enter your choice: ");
+            answer = Convert.ToInt32(Console.ReadLine());
+            if (answer == 1)
+                score += 20;
+
+            Console.WriteLine("\nQ5. What is the national game of England ");
+            Console.WriteLine("1.Rugby  2.Cricket 3.Football 4.Basketball");
+            Console.Write("Enter your choice: ");
+            answer = Convert.ToInt32(Console.ReadLine());
+            if (answer == 2)
+                score += 20;
+            //Adding Score to File
+            if (File.Exists(fileName))
+                File.Delete(fileName);
+            Console.WriteLine("\n*****File Deleted*****\n");
+
+            StreamWriter sw = File.CreateText(fileName);
+            sw.WriteLine(name);
+            sw.WriteLine(score);
+            sw.Close();
+            Console.WriteLine("*****Score Added*****");
+            Console.WriteLine($"\nThanks, {name} score was added to our company server");
+
+            Console.ReadLine();
+        }
+    }
+}
