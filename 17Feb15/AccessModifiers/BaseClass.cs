33055b9c632a439e390aa6e3331ebc74f6daad95
SuryaTejaCh2 <suryatejach2@gmail.com>
2022-02-15 15:57:52 +0530

    Feb15

---
+++ 17Feb15/AccessModifiers/BaseClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    /// <summary>
+    /// Base Class
+    /// </summary>
+    public class BaseClass
+    {
+        public int a;
+        private int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+        /// <summary>
+        /// Base Class Method
+        /// </summary>
+        public void BaseClassMethod()
+        {
+            a = 10;
+            b = 20;
+            c = 30;
+            d = 40;
+            e = 50;
+        }
+    }
+    /// <summary>
+    /// Derived Class
+    /// </summary>
+    public class DerivedClass : BaseClass
+    {
+        /// <summary>
+        /// Derived Class Method
+        /// </summary>
+        public void DerivedClassMethod()
+        {
+            a = 10;
+            //b = 20; Private variable cannot accessble in same derived class
+            c = 30;
+            d = 40;
+            e = 50;
+        }
+    }
+    /// <summary>
+    /// Main Class
+    /// </summary>
+    public class MainClass
+    {
+        /// <summary>
+        /// Base Class Object Created
+        /// </summary>
+        BaseClass bc = new BaseClass();
+        /// <summary>
+        /// Main Class Method
+        /// </summary>
+        public void MainClassMethod()
+        {
+            bc.a = 10;
+            //bc.b = 20; Private variable cannot accessble in same main class
+            //bc.c = 30; Protected variable cannot accessble in same main class
+            bc.d = 40;
+            bc.e = 50;
+        } 
+    }
+}
---
+++ 17Feb15/AccessModifiers2/OtherDerivedClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccessModifiers;
+
+namespace AccessModifiers2
+{
+    /// <summary>
+    /// Other Derived Class
+    /// </summary>
+    public class OtherDerivedClass : BaseClass
+    {
+        /// <summary>
+        /// Other Derived Class Method
+        /// </summary>
+        public void OtherDerivedClassMethod()
+        {
+            a = 10;
+            //b = 20; Private variable cannot accessble in other derived class
+            c = 30;
+            //d = 40; Internal variable cannot accessble in other derived class
+            e = 50;
+        }
+    }
+    /// <summary>
+    /// Other Main Class
+    /// </summary>
+    public class OtherMainClass
+    {
+        BaseClass bc2 = new BaseClass();
+        /// <summary>
+        /// Other Main Class Method
+        /// </summary>
+        public void OtherMainClassMethod()
+        {
+            bc2.a = 10;
+            //bc2.b = 20; Private variable cannot accessble in other main class
+            //bc2.c = 30; Protected variable cannot accessble in other main class
+            //bc2.d = 40; Internal variable cannot accessble in other main class
+            //bc2.e = 50; Protected Internal variable cannot accessble in other main class
+        }
+    }
+}
